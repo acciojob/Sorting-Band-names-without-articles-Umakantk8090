5db83428ef2c46871d6b9a477492ab4c8934c69b
Umakantk8090 <117897072+Umakantk8090@users.noreply.github.com>
2023-10-13 19:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots.sort((a,b) =>{
+	if(a>b){
+		return -1;
+	}
+	if(a<b){
+		return -1;
+	}
+	return 0;
+})
+console.log(touristSpots);
